26ac13722fd75f6894b9a1cfc55551c39795f1ce
Baktajivan Pillay 252709 <baktajivan98@gmail.com>
2019-12-19 07:23:23 +0800

    Update AlphabetOnly.java

--- CBD2/src/main/java/com/baktajivan/AlphabetOnly.java
+++ CBD2/src/main/java/com/baktajivan/AlphabetOnly.java
@@ -5,7 +5,13 @@ public class AlphabetOnly {
     //create the method
     public void Compute(String input){
         String output = input.replaceAll("[^a-zA-Z]","");//just keeps the alphabet only
-        System.out.println("Output: "+output);//prints the output
+        if(output.equals("")) {
+            System.out.println("Output: There is no alphabet in the input string");//prints the output if no alphabet detected
+        }
+        else {
+            System.out.println("Output: " + output);//prints the output
+        }
     }//method end
 }//class end
+
 

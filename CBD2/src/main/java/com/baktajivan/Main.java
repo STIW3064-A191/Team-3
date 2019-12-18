c999208d0d61a7cc1ffa1502368256ad96ed8999
Baktajivan Pillay 252709 <baktajivan98@gmail.com>
2019-12-19 06:02:43 +0800

    Update Main.java

--- CBD2/src/main/java/com/baktajivan/Main.java
+++ CBD2/src/main/java/com/baktajivan/Main.java
@@ -1,10 +1,8 @@
 package com.baktajivan;
-
+//code are executed here
 public class Main {
     public static  void main (String  [] args){
-        Scan sc = new Scan();
+        Scan sc = new Scan();//create a constructor
         sc.scanner();//run all the methods in the scanner
-
-
-    }
-}
+    }//method end
+}//class end
